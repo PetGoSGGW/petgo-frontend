7cd4c111fa16858ee2c507a34204369381248109
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2026-01-04 08:22:13 +0100

    PGF-20

--- src/app/features/walker-offers/services/walker-offers-api.service.ts
+++ src/app/features/walker-offers/services/walker-offers-api.service.ts
@@ -16,11 +16,6 @@ export class WalkerOffersApiService {
     longitude: number;
     radiusKm: number;
   }): Observable<{ content: WalkerOffer[]; totalElements: number; number: number }> {
-    // console.log(params);
-    // return this.http.get<{ content: WalkerOffer[]; totalElements: number; number: number }>(
-    //   '/assets/mock/walker-offers-search.json',
-    // );
-
     return this.http.get<{ content: WalkerOffer[]; totalElements: number; number: number }>(
       `${this.apiUrl}/offers/search`,
       { params },
