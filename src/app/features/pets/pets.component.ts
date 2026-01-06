1fbe8833f8d05a89543922d924f5c2910259b50f
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2026-01-06 21:07:56 +0100

    PGF-19

--- src/app/features/pets/pets.component.ts
+++ src/app/features/pets/pets.component.ts
@@ -1,19 +1,30 @@
 import { Component, computed, inject } from '@angular/core';
 import { DogApiService } from '../../services/dog-api.service';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { RouterModule } from '@angular/router';
 import { AuthService } from '../../core/auth/services/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { AddDogFormDialogComponent } from '../home/components/add-dog-form-dialog/add-dog-form-dialog.component';
 import { filter } from 'rxjs';
+import { DogsGridComponent } from '../../components/dogs-grid/dogs-grid.component';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatError, MatHint } from '@angular/material/form-field';
+import { SectionWrapperComponent } from '../../components/section-wrapper/section-wrapper.component';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-pets-component',
-  imports: [RouterModule],
+  imports: [
+    DogsGridComponent,
+    MatProgressSpinner,
+    MatError,
+    MatHint,
+    SectionWrapperComponent,
+    MatButton,
+  ],
   templateUrl: './pets.component.html',
   styleUrl: './pets.component.css',
 })
-export class PetsComponent {
+export default class PetsComponent {
   private authService = inject(AuthService);
   private readonly dogApi = inject(DogApiService);
   private readonly matDialog = inject(MatDialog);
--- src/app/features/pets/pets.routes.ts
+++ src/app/features/pets/pets.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const petsRoutes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pets.component').then((c) => c.PetsComponent),
+    loadComponent: () => import('./pets.component'),
   },
 ];
